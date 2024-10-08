ff258c8e3fa2609c9f5e59266a9d292ed43c795c
Alvarokstar <afercal1704@fp.iesromerovargas.com>
2024-10-08 09:32:44 +0200

    ejercicio 12

--- cadena/ejercicio_2.py
+++ cadena/ejercicio_2.py
@@ -7,3 +7,5 @@ for i in range(len(nombre)):
     cont += 1
 
 print(cont)
+
+#Pide al usuario su nombre completo por consola y este muestra los carácteres de dicho nombre por la consola.
--- cadena/ejercicio_3.py
+++ cadena/ejercicio_3.py
@@ -3,3 +3,5 @@ nombre = "Lorena"
 print(nombre.upper())
 print(nombre.lower())
 print(nombre.capitalize())
+
+#Muestra el nombre seleccionado en mayúsculas, minúsculas y con la primera letra en mayúscula.

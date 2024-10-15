2c9bd53e276cffc7fadde743c08a0fc040805ad6
Alvarokstar <afercal1704@fp.iesromerovargas.com>
2024-10-15 12:28:28 +0200

    mas ejercicios

---
+++ condicional_if/ejercicio_34.py
@@ -0,0 +1,4 @@
+num = int(input("Dime tu edad: "))
+print(not 18 + "Es mayor de 18" >= num)
+
+
---
+++ tuplas/ejercicio_45.py
@@ -0,0 +1,18 @@
+from traceback import print_tb
+
+set1 = {1,2,3,4,5}
+
+print(set1)
+
+set1.add(6)
+
+print(set1)
+
+set1.discard(3)
+
+print(set1)
+
+print(2 in set1)
+
+print(set1)
+
